771d12c7defd460cfff09dc29302fc2993470a10
Gyro Dev <gyrodefi@gmail.com>
2022-01-14 19:51:56 +0800

    chg: disable analytics.

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require('@sentry/nextjs')
+// const { withSentryConfig } = require('@sentry/nextjs')
 
 const nextConfig = {
   webpack: (config) => {
@@ -158,7 +158,8 @@ const SentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(withPWA(withBundleAnalyzer(nextConfig)), SentryWebpackPluginOptions)
+// module.exports = withSentryConfig(withPWA(withBundleAnalyzer(nextConfig)), SentryWebpackPluginOptions)
+module.exports = withPWA(withBundleAnalyzer(nextConfig))
 
 // Don't delete this console log, useful to see the config in Vercel deployments
 console.log('next.config.js', JSON.stringify(module.exports, null, 2))
